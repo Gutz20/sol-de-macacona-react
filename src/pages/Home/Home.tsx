2d92f7bd06ea8ec3f064d6f83d3273ee880b553c
Daniel S <mitmacontreras@gmail.com>
2023-09-01 14:22:48 -0500

    fondo del home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@mui/material";
-
 const Home = () => (
-  <div>
-    <h1 className="font-bold text-3xl"></h1>
-    <Button variant="text"></Button>
+  <>
+  <div className="bg-cover bg-fondo1 h-[800px] bg-center">
+    <div className=" h-50px w-auto place-items-center top-50% left-50% bg-white text-orange-950 py-20 px-20 grid opacity-60 left-80 top-96 text-5xl absolute font-serif drop-shadow-2xl">
+      <h2>Descubre el Placer de vivir</h2>
+      <h2> en el entorno perfecto</h2>
+    </div>  
   </div>
+ <div>
+  
+  </div> 
+  </>
 );
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "fondo1":"url('/imagen-index.jpg')"
+      }
+    },
   },
   plugins: [],
 };
