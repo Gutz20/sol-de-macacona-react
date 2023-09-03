2ab8266b430a2bb739c419d1996987f716dfe2a3
Joe <darckProyect8@gmail.com>
2023-09-02 22:46:56 -0500

    Responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,5 @@
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-import { A11y, Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import { Formulario } from "@/components";
+import { HomeCarrusel } from ".";
 
 const Home = () => {
   return (
@@ -28,12 +22,12 @@ const Home = () => {
                  max-sm:container max-sm:mx-auto  max-sm:flex max-sm:justify-center max-sm:gap-1"
         >
           <div
-            className="mx-40 flex gap-4 
-          max-sm:flex-col max-sm:gap-4 max-sm:mx-10 max-sm:items-center "
+            className="lg:mx-40 mx-20 flex gap-4 
+          max-sm:flex-col max-sm:gap-4 max-sm:mx-10 max-sm:items-center max-lg:flex-col"
           >
             <img
-              className="bg-cover bg-fam h-76 w-50 max-sm:h-[220px] max-sm:w-76 "
-              src="/public/fam.png"
+              className="bg-cover h-76 w-50 max-sm:h-[220px] max-sm:w-76"
+              src="./family-home-section.svg"
               alt="familia"
             ></img>
             <div className="inline-block">
@@ -59,68 +53,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className=" bg-neutral-700 py-20">
-        <div className="container mx-auto">
-          <div className="flex items-center justify-center flex-col gap-8">
-            <Swiper
-              effect={"coverflow"}
-              spaceBetween={50}
-              grabCursor={true}
-              centeredSlides={true}
-              slidesPerView={"auto"}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Autoplay, Pagination, Navigation, A11y]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <img src="./casa1.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="./casa2.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="./casa3.jpg" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="./casa4.jpg" />
-              </SwiperSlide>
-            </Swiper>
-            <div className="flex gap-8 justify-center max-sm:flex-col max-sm:gap-2 ">
-              <div className="text-slate-50 text-center">
-                <div className="max-sm:w-full max-sm:bg-gray-400 max-sm:h-[0.5px] max-sm:mb-5 max-sm:mt-4"></div>
-                <h3 className="text-3xl font-bold max-sm:text-xs max-sm:font-bold">
-                  +10 años
-                </h3>
-                <p className="font-semibold text-xl max-sm:text-xs">
-                  de experiencia en el sector
-                </p>
-              </div>
-              <div className="w-[0.50px] bg-white"></div>
-              <div className="text-slate-50 text-center">
-                <div className="max-sm:w-full max-sm:bg-gray-400 max-sm:h-[0.5px] max-sm:mb-5 max-sm:mt-4"></div>
-                <h3 className="text-3xl font-bold max-sm:text-xs max-sm:font-bold">
-                  +100 departamentos
-                </h3>
-                <p className="font-semibold text-xl max-sm:text-xs">
-                  de experiencia en el sector
-                </p>
-              </div>
-              <div className="w-[0.50px] bg-white"></div>
-              <div className="text-slate-50 text-center">
-                <div className="max-sm:w-full max-sm:bg-gray-400 max-sm:h-[0.5px] max-sm:mb-5 max-sm:mt-4"></div>
-                <h3 className="text-3xl font-bold max-sm:text-xs max-sm:font-bold">
-                  +20 aliados
-                </h3>
-                <p className="font-semibold text-xl max-sm:text-xs">
-                  trabajando con nosotros
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HomeCarrusel />
       <div
         className="bg-cover bg-atras h-[500px] bg-center relative flex py-10
                       max-sm:h-[350px]"
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export * from './About';
+export * from './Home/components/HomeCarrusel';
 export * from './Home';
 export * from './Projects-For-Sale';
 export { default as Ubication } from './Ubication/Ubication';
