4454a5dd930d9bff792f1026ba810780e41bf396
AlessPe <alesandrocaveroalvarez474@gmail.com>
2023-09-20 13:15:21 -0500

    avance_aless_6

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -29,10 +29,10 @@ export const slidesHome = [
 ];
 
 export const slidesProjectForSale = [
-  { id: 1, src: "./casa2.jpg", alt: "casa 1" },
+  { id: 1, src: "./modelos-general.jpg", alt: "casa 1" },
   { id: 2, src: "./casa2.jpg", alt: "casa 2" },
   { id: 3, src: "./casa3.jpg", alt: " casa 3" },
-  { id: 4, src: "./casa3.jpg", alt: "casa 4" },
+  { id: 4, src: "./casa5.jpg", alt: "casa 4" },
 ];
 
 export const ourTeam = [
